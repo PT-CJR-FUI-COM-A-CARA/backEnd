e1807d6c6d47570dd98fca232afc3cb76a65119c
pedroiaan <pedroianx8@gmail.com>
2025-06-09 21:45:52 -0300

    feat: adição das estratégias

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+import * as bcrypt from 'bcrypt';
+
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private readonly userService: UsersService) {}
+    async validateUser(email: string, senha: string){
+        const user = await this.userService.findByEmail(email);
+        
+        if (user) {
+            const senhaValida = await bcrypt.compare(senha, user.senha)
+            if (senhaValida){
+            return {
+                ...user,
+                senha: undefined,
+            }
+        };
+        }
+        throw new Error('email ou senha inválida')
+    }
+}
