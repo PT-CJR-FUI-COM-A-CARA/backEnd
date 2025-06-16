992e807ce6996ab723c455b36d04d85481bb7d95
pedroiaan <pedroianx8@gmail.com>
2025-06-16 14:16:10 -0300

    fix: ajustes das permissões

--- src/avaliacoes/avaliacoes.controller.ts
+++ src/avaliacoes/avaliacoes.controller.ts
@@ -16,12 +16,12 @@ import { AvaliacoesDtoUpdate } from './dto/update-avaliacoes.dto';
 @Controller('avaliacoes')
 export class AvaliacoesController {
     constructor(private readonly avaliacoesService: AvaliacoesService) {}
-    @IsPublic()
+    
     @Post()
     async create(@Body() data: AvaliacoesDto) {
         return this.avaliacoesService.create(data)
     }
-    
+    @IsPublic()
     @Get()
     async findAll(){
         return this.avaliacoesService.findAll();
@@ -31,7 +31,7 @@ export class AvaliacoesController {
     async delete(@Param('id', ParseIntPipe) id: number){
         return this.avaliacoesService.delete(id);
     }
-    @IsPublic()
+    
     @Patch(':id')
     async update (@Param('id', ParseIntPipe) id: number, @Body() updateData: AvaliacoesDtoUpdate) {
         return this.avaliacoesService.update(id, updateData)
