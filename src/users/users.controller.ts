19e4428d77da57d7607e732b948982c0824b8bfc
Joao Victor Felix Moreira <231037709@aluno.unb.br>
2025-07-10 22:40:14 -0300

    :sparkles:fix: Adicionando o modal de editar perfil

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -12,18 +12,29 @@ import { UsersDto } from './dto/users.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { IsPublic } from 'src/auth/decorators/is-public.decorator';
+import { ChangePasswordDto } from './dto/change-password.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly userService: UsersService) {}
+
   @IsPublic()
   @Post()
   async create(@Body() data: UsersDto) {
     return this.userService.create(data);
   }
+
   @Get()
   async findAll() {
     return this.userService.FindAll();
+  }
+
+  @Patch('change-password/:id')
+  async changePassword(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() changePasswordDto: ChangePasswordDto,
+  ) {
+    return this.userService.changePassword(id, changePasswordDto);
   }
 
   @IsPublic()
@@ -35,9 +46,13 @@ export class UsersController {
   @Patch(':id')
   async update(
     @Param('id', ParseIntPipe) id: number,
-    @Body() updateData: UpdateUserDto,) {
+    @Body() updateData: UpdateUserDto,
+  ) {
+        console.log(`--- TENTANDO ATIVAR CHANGE user com ID: ${id} ---`);
+
     return this.userService.update(id, updateData);
   }
+
   @Delete(':id')
   async delete(@Param('id', ParseIntPipe) id: number) {
     return this.userService.delete(Number(id));
